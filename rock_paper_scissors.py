67726d8171cd8fb14bbb7c878c379d75d8c6bfa8
kokonutzlabs <kortneyd24@gmail.com>
2025-10-06 22:55:04 -0400

    Update rock_paper_scissors.py
    # Update to Code
    ###### Made this code into a loop

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,9 +2,8 @@
 print("Rock, Paper, scissors")
 
 import random
-player = 0
-computer = 0
 score = 0
+computer = random.randint(1,3)
 
 player = int(input('''Choose your hand (1-3):
 1. Rock
@@ -12,58 +11,30 @@ player = int(input('''Choose your hand (1-3):
 3. Scissors
 4. Quit
  '''))
+ 
 
-computer = random.randint(1,3)
-
-if computer == 1 and player == 3:
-  print("rock beats scissor, you lost.")
-  player = int(input('''Choose your hand (1-3):
+while player in [1,2,3]:
+    player = int(input('''Choose your hand (1-3):
 1. Rock
 2. Paper
 3. Scissors
 4. Quit
  '''))
-elif computer == 2 and player == 1:
-  print("paper beats rock, you lost.")
-  player = int(input('''Choose your hand (1-3):
-1. Rock
-2. Paper
-3. Scissors
-4. Quit
- '''))
-elif computer == 1 and player == 1:
-  print("its a tie, try again.")
-  player = int(input('''Choose your hand (1-3):
-1. Rock
-2. Paper
-3. Scissors
-4. Quit
- '''))
-elif computer == 2 and player == 2:
-  print("its a tie, try again.")
-  player = int(input('''Choose your hand (1-3):
-1. Rock
-2. Paper
-3. Scissors
-4. Quit
- '''))
-elif computer == 3 and player == 3:
-  print("its a tie, try again.")
-  player = int(input('''Choose your hand (1-3):
-1. Rock
-2. Paper
-3. Scissors
-4. Quit
- '''))
-else:
-  print("Yay, you won!!")
-  score != +1
-  player = int(input('''Choose your hand (1-3):
-1. Rock
-2. Paper
-3. Scissors
-4. Quit
- '''))
+ 
+    if computer == player:
+        print("its a tie, try again.")
+        
+    if player in [1, 2, 3]:
+        if computer == 1 and player == 3:
+            print(f'rock beats scissor, you lost.')
+        elif computer == 2 and player == 1:
+            print(f'paper beats rock, you lost.')
+        elif computer == 3 and player == 2:
+            print(f'Scissors beats paper, you lost.')
+        else:
+            print("Yay, you won!!")
+            score != +1
 
 if player == 4:
   print(f'Your score is: {score} pts, bye bye!')
+
