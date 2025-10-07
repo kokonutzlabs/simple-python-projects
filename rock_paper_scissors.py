556139fbdd8cfe09456133e5338e64ca9679a2d8
kokonutzlabs <kortneyd24@gmail.com>
2025-10-06 22:53:00 -0400

    Update and rename if rock, paper, scissors.py to rock_paper_scissors.py
    # Update to Code
    ###### slight change to if conditions.

---
+++ rock_paper_scissors.py
@@ -0,0 +1,69 @@
+#make this into a while loop later
+print("Rock, Paper, scissors")
+
+import random
+player = 0
+computer = 0
+score = 0
+
+player = int(input('''Choose your hand (1-3):
+1. Rock
+2. Paper
+3. Scissors
+4. Quit
+ '''))
+
+computer = random.randint(1,3)
+
+if computer == 1 and player == 3:
+  print("rock beats scissor, you lost.")
+  player = int(input('''Choose your hand (1-3):
+1. Rock
+2. Paper
+3. Scissors
+4. Quit
+ '''))
+elif computer == 2 and player == 1:
+  print("paper beats rock, you lost.")
+  player = int(input('''Choose your hand (1-3):
+1. Rock
+2. Paper
+3. Scissors
+4. Quit
+ '''))
+elif computer == 1 and player == 1:
+  print("its a tie, try again.")
+  player = int(input('''Choose your hand (1-3):
+1. Rock
+2. Paper
+3. Scissors
+4. Quit
+ '''))
+elif computer == 2 and player == 2:
+  print("its a tie, try again.")
+  player = int(input('''Choose your hand (1-3):
+1. Rock
+2. Paper
+3. Scissors
+4. Quit
+ '''))
+elif computer == 3 and player == 3:
+  print("its a tie, try again.")
+  player = int(input('''Choose your hand (1-3):
+1. Rock
+2. Paper
+3. Scissors
+4. Quit
+ '''))
+else:
+  print("Yay, you won!!")
+  score != +1
+  player = int(input('''Choose your hand (1-3):
+1. Rock
+2. Paper
+3. Scissors
+4. Quit
+ '''))
+
+if player == 4:
+  print(f'Your score is: {score} pts, bye bye!')
